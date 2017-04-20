a3c26ca91bb44fcda0d7002878a77ce8c9f4dd21
Elmer Ibayan <eibayan@gmail.com>
2017-04-21 09:15:35 +1000

    refactor product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,15 @@
 class ProductsController < ApplicationController
+  
+  before_action :get_pricelist, only: [:index]
+  
   def index
-    response = HTTParty.get('https://api.myjson.com/bins/gx6vz')
-    data = JSON.parse(response.body)
-    @products = data["prices"]
+    @products
   end
+
+  private
+    def get_pricelist
+      response = HTTParty.get('https://api.myjson.com/bins/gx6vz')
+      data = JSON.parse(response.body)
+      @products = data["prices"]
+    end
 end
